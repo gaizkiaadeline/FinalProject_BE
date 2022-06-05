2cb73b53c17af8d962365cbd16d5c294a79d344e
Gaizkia Adeline Atmaka <gaizkiaadelinee@gmail.com>
2022-06-06 00:08:37 +0700

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Dashboard | PT Musang')
 
 @section('content')
-    <h1>Hello {{ Auth::user()->fullname }} !</h1>
+    <h1>Hello {{ Auth::user()->fullname }} !!!</h1>
     <h3>Your Role is {{ Auth::user()->role }}</h3>
 
 @endsection
